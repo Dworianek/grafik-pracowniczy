40f34760583486fd20dbd3a16c482951817f2456
Adrian Dworak <adrian.dworak97@gmail.com>
2023-02-11 11:44:13 +0100

    Updates

---
+++ precache-manifest.8a5fe2c203734020a0f3b2bbc2b99169.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6cf3eb570727de34a62",
+    "url": "/grafik-pracowniczy/static/css/main.dc2ce5d0.chunk.css"
+  },
+  {
+    "revision": "e6cf3eb570727de34a62",
+    "url": "/grafik-pracowniczy/static/js/main.e6cf3eb5.chunk.js"
+  },
+  {
+    "revision": "1314b5dd1d862d4ffa41",
+    "url": "/grafik-pracowniczy/static/css/1.b9e5e133.chunk.css"
+  },
+  {
+    "revision": "1314b5dd1d862d4ffa41",
+    "url": "/grafik-pracowniczy/static/js/1.1314b5dd.chunk.js"
+  },
+  {
+    "revision": "cf46d90b1a7be1e36f4f",
+    "url": "/grafik-pracowniczy/static/js/runtime~main.cf46d90b.js"
+  },
+  {
+    "revision": "4c03e09ef8de04d382907e26d8452589",
+    "url": "/grafik-pracowniczy/static/media/trash.4c03e09e.svg"
+  },
+  {
+    "revision": "fea006c3b84fb489211abbe9afdc68a6",
+    "url": "/grafik-pracowniczy/static/media/error404.fea006c3.png"
+  },
+  {
+    "revision": "6be9ca3b73fafaa6219b33327e276137",
+    "url": "/grafik-pracowniczy/static/media/password.6be9ca3b.png"
+  },
+  {
+    "revision": "0bae4bc0c429fcc57399164b74d87aeb",
+    "url": "/grafik-pracowniczy/static/media/homepage.0bae4bc0.png"
+  },
+  {
+    "revision": "496b19c6543ea09e55ad4e25fac16d97",
+    "url": "/grafik-pracowniczy/index.html"
+  }
+];
